33b58ef0c2bbc3cf3f9da6470f0adf44bf6ecf49
Sai Sreeprada T <72294583+Sreeprada6303@users.noreply.github.com>
2023-04-07 17:46:16 +0530

    Add files via upload

---
+++ c++codeexample3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int power_level = 9000;
+    
+    cout << "power_level";
+    cout << " over ";
+    cout << power_level;
+    
+    return 0;
+}
