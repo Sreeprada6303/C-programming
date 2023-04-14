ba7f112a61678c9651686ca15a02e29ec0496a57
Sai Sreeprada T <72294583+Sreeprada6303@users.noreply.github.com>
2023-04-14 12:02:43 +0530

    Add files via upload

---
+++ c++codeexample7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // create salary variable and assign 1500 to it
+    int salary = 1500;
+ 
+    // print the salary variable
+    cout << salary << endl;
+
+    // assign 3000 to salary
+    salary = 3000;
+
+    // print the salary variable again in a new line
+    cout << salary;
+
+    return 0;
+}
