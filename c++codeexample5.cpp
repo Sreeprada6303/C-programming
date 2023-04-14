a91344debe0608a0a8e6948d0f6e5f325ab1dcc8
Sai Sreeprada T <72294583+Sreeprada6303@users.noreply.github.com>
2023-04-14 11:41:35 +0530

    Add files via upload

---
+++ c++codeexample5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    
+    double number = 78.34;
+    char alphabet = 'u';
+    
+    
+    cout << number << endl;
+    cout << alphabet;
+    
+    return 0;
+}
