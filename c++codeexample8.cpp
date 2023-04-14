5c75e1342795064e0b1d4eb00e8272c5c5bb9733
Sai Sreeprada T <72294583+Sreeprada6303@users.noreply.github.com>
2023-04-14 12:15:27 +0530

    Add files via upload

---
+++ c++codeexample8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // create and print the distance variable
+    int distance = 134;
+    cout << distance << endl;
+    
+    // create another variable distance2
+    int distance2 = 564;
+    
+    // assign the value of distance2 to distance
+    distance = distance2;
+    
+    // print distance
+    cout << distance;
+    
+    return 0;
+}
