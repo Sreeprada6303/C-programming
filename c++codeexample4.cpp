22486da8ee7f6fae3d6bf697b04eb7a3ee4235f6
Sai Sreeprada T <72294583+Sreeprada6303@users.noreply.github.com>
2023-04-14 11:28:19 +0530

    Add files via upload

---
+++ c++codeexample4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // create two variables
+    int number1 = 89;
+    double number2 = 313.78;
+    
+    // print variables
+    cout << number1 << endl;
+    cout << number2;
+    
+    return 0;
+}
