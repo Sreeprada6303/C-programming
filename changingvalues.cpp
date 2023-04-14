422f6af8fb44641db4294fb989db65836debde2a
Sai Sreeprada T <72294583+Sreeprada6303@users.noreply.github.com>
2023-04-14 11:45:08 +0530

    Add files via upload

---
+++ changingvalues.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+    // create a variable with value 25
+    int age = 25;
+    cout << age << endl;
+ 
+    // change value from 25 to 19
+    age = 19;
+    cout << age;
+ 
+    return 0;
+}
